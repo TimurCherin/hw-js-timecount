9c5d0ac11ba2aa3854f78977e5d309870ff4b951
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2024-02-28 21:23:19 +0200

    s

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const days = document.querySelector(".days")
 const hours = document.querySelector(".hours")
 const minutes = document.querySelector(".minutes")
 const seconds = document.querySelector(".seconds")
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
 const currentTime = prompt("Введіть час у секундах")
-let timeDays =  Math.floor(currentTime / 60 / 60 / 24);
+let timeDays = addLeadingZero(Math.floor(currentTime / 60 / 60 / 24)) 
 let timeDayRemainder = Math.floor(currentTime % (60 * 60 * 24))
-let timeHours =  Math.floor(timeDayRemainder / 60 / 60);
+let timeHours = addLeadingZero(Math.floor(timeDayRemainder / 60 / 60)) 
 let timeHoursRemainder = Math.floor(currentTime % (60 * 60))
-let timeMinutes =  Math.floor(timeHoursRemainder / 60);
+let timeMinutes =  addLeadingZero(Math.floor(timeHoursRemainder / 60))
 let timeMinutesRemainder = Math.floor(currentTime % (60))
-let timeSeconds =  Math.floor(timeMinutesRemainder);
+let timeSeconds =  addLeadingZero(Math.floor(timeMinutesRemainder))
 setInterval(changeSeconds, 1000)
 setInterval(countSeconds, 1000)
 function changeSeconds(){
@@ -19,6 +22,9 @@ function changeSeconds(){
 function countSeconds (){
     if (timeSeconds >= 1) {
         timeSeconds --
+    } 
+    if (timeSeconds === 10) {
+        timer.classList.add("animation")
     } 
     if (timeMinutes >= 1 && timeSeconds === 0){
         timeSeconds = 60
